7903b773977e362150a088435a4d5fc8d08f518e
Natalie <natassshaka@MacBook-Air-Natalie.local>
2022-10-09 23:34:57 +0300

    Initial commit

---
+++ HomeWork4.py
@@ -0,0 +1,14 @@
+#1. Вычислить число c заданной точностью d
+# Пример:
+# - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+
+# def Pi(number: float='')-> int:
+#     s = str(number)
+#     d=abs(s.find('.') - (len(s)) +1)
+#     from math import pi
+#     p=round(pi,d)
+#     print(f' Число Пи с заданной точностью {d} равно {round(pi, d)}')
+
+# N=input('Введите число: ')
+# Pi(N)
